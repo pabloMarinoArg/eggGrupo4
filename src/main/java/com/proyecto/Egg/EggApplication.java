9b72fae8d331e69098e0a8af4286b404fa8cba8f
pabloMarinoArg <31575175+pabloMarinoArg@users.noreply.github.com>
2021-06-15 20:32:17 -0300

    primercommitd

---
+++ src/main/java/com/proyecto/Egg/EggApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.Egg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/Egg/EggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.Egg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
