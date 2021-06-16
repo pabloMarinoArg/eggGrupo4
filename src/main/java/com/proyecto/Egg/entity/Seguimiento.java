5dd341d3ac5b28d97706109e050823b9c5ce0af2
pabloMarinoArg <31575175+pabloMarinoArg@users.noreply.github.com>
2021-06-15 21:24:08 -0300

    agregoEntitySeguimiento

---
+++ src/main/java/com/proyecto/Egg/entity/Seguimiento.java
@@ -0,0 +1,92 @@
+package com.proyecto.Egg.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Seguimiento {
+
+	@Id
+	@GeneratedValue(generator="uuid")
+    @GenericGenerator(name="uuid",strategy = "uuid2")
+	private String id;
+	private String comentario;
+	@Temporal(TemporalType.DATE)
+	private Date fecha;
+	@OneToOne
+	private Usuario usuario;
+	@ManyToOne
+	private Medico medico;
+	
+	
+	
+	public Seguimiento() {
+		
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Medico getMedico() {
+		return medico;
+	}
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seguimiento other = (Seguimiento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+}
