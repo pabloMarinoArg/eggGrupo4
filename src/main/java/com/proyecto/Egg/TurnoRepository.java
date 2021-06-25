463b7f29583ca8a7955396d1b882bed6e5191105
gustamarin <82985097+gustamarin@users.noreply.github.com>
2021-06-25 20:37:51 -0300

    Add files via upload

---
+++ src/main/java/com/proyecto/Egg/TurnoRepository.java
@@ -0,0 +1,20 @@
+package com.proyecto.Egg.repository;
+
+import com.proyecto.Egg.entity.Turno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TurnoRepository extends JpaRepository<Turno, Long> {
+
+    @Modifying
+    @Query("UPDATE Turno t SET t.fecha = :fecha, t.hora= :hora, t.mail = :mail WHERE u.id = :id")
+    void modificar(@Param("id") String id, @Param("username") String username, @Param("password") String password);
+
+    //@Query("SELECT u FROM Usuario u WHERE u.username = :username")
+    //Turno buscarPorPaciente(@Param("pacienteVisitado") PacienteVisitado pacienteVisitado);
+
+}
